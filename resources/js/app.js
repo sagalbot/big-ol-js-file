b062c1371104ba4f252cd2a5e34ee09d9fe97479
Jeff <sagalbot@gmail.com>
2020-11-03 16:23:07 -0800

    install inertia client side

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
-require('./bootstrap');
+import { createApp, h } from 'vue';
+import { App, plugin } from '@inertiajs/inertia-vue3';
+
+const el = document.getElementById('app');
+
+const app = createApp({
+  render: () => h(App, {
+    initialPage: JSON.parse(el.dataset.page),
+    resolveComponent: name => require(`./Pages/${name}`).default,
+  }),
+});
+
+app.use(plugin).mount(el);
